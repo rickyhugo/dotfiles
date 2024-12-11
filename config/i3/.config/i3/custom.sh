ecd8a05db6e8b1a9acf6708fc53628f10e7544c4
hugo-englund <hugo.englund@schibsted.com>
2024-12-11 09:38:23 +0100

    :horse:

--- config/i3/.config/i3/custom.sh
+++ config/i3/.config/i3/custom.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # keyboard
-setxkbmap -layout us,se -option 'grp:alt_shift_toggle' -option compose:ralt
+setxkbmap -layout us,se -option 'grp:alt_shift_toggle' -option compose:ralt # define compose key in order to enable world layer
 
 # key repeat (<milliseconds_before_repeating> <repetitions_per_second>)
 xset r rate 220 150
 
 # touchpad
-touchpad="Synaptics TM3625-010"
+touchpad="VEN_04F3:00 04F3:311C Touchpad"
 xinput set-prop "$touchpad" "libinput Natural Scrolling Enabled" 1
-xinput set-prop "$touchpad" "libinput Accel Speed" 0.75
 xinput set-prop "$touchpad" "libinput Tapping Enabled" 1
 
 # proper screen locking
