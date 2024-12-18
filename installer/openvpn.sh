691c42468f48db3404c132de7ad3bf83741c23ad
rickyhugo <hugo.englund7@gmail.com>
2024-12-18 08:33:15 +0100

    :horse:

---
+++ installer/openvpn.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+curl -O https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh
+chmod +x openvpn-install.sh
+sudo ./openvpn-install.sh
+rm openvpn-install.sh
