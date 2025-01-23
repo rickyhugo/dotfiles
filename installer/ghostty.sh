2eb02781969629ef53b5e741932534fccb317334
rickyhugo <hugo.englund7@gmail.com>
2025-01-23 07:40:40 +0100

    :horse:

---
+++ installer/ghostty.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source /etc/os-release
+ARCH=$(dpkg --print-architecture)
+GHOSTTY_DEB_URL=$(
+  curl -s https://api.github.com/repos/mkasberg/ghostty-ubuntu/releases/latest |
+    grep -oP "https://github.com/mkasberg/ghostty-ubuntu/releases/download/[^\s/]+/ghostty_[^\s/_]+_${ARCH}_${VERSION_ID}.deb"
+)
+GHOSTTY_DEB_FILE=$(basename "$GHOSTTY_DEB_URL")
+curl -LO "$GHOSTTY_DEB_URL"
+sudo dpkg -i "$GHOSTTY_DEB_FILE"
+rm "$GHOSTTY_DEB_FILE"
