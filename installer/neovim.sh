8362030ce647d85ba82d56e3d4a982e9869dd769
Hugo Englund <hugo.englund7@gmail.com>
2025-02-07 12:02:31 +0100

    :horse:

--- installer/neovim.sh
+++ installer/neovim.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-sudo mv nvim.appimage /usr/local/bin/nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+chmod u+x nvim-linux-x86_64.appimage
+mv nvim-linux-x86_64.appimage ~/.local/bin/nvim
 nvim --version
