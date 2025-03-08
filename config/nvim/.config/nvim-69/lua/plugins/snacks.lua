8c1ea4afa30e165bec0d86b51249cf80ebfd65a4
rickyhugo <hugo.englund7@gmail.com>
2025-03-08 02:39:31 +0100

    :horse:

---
+++ config/nvim/.config/nvim-69/lua/plugins/snacks.lua
@@ -0,0 +1,55 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		bigfile = { enabled = false },
+		dashboard = { enabled = false },
+		explorer = { enabled = false },
+		indent = { enabled = false },
+		input = { enabled = false },
+		picker = { enabled = true },
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
+		scope = { enabled = false },
+		scroll = { enabled = false },
+		statuscolumn = { enabled = false },
+		words = { enabled = false },
+		dim = { enabled = false },
+		zen = { enabled = true },
+
+		styles = {
+			zen = {
+				backdrop = { transparent = true, blend = 15 },
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader>z",
+			function()
+				Snacks.zen()
+			end,
+			desc = "Toggle Zen Mode",
+		},
+		{
+			"<leader>N",
+			desc = "Neovim News",
+			function()
+				Snacks.win({
+					file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+					width = 0.6,
+					height = 0.6,
+					wo = {
+						spell = false,
+						wrap = false,
+						signcolumn = "yes",
+						statuscolumn = " ",
+						conceallevel = 3,
+					},
+				})
+			end,
+		},
+	},
+}
