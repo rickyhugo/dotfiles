8046213baad5f6231a35e55da4a1618179451578
rickyhugo <hugo.englund7@gmail.com>
2025-04-20 16:57:47 +0200

    :horse:

--- config/nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -16,54 +16,39 @@ return {
 				git_placement = "after",
 			},
 		},
-	},
-	config = function(_, opts)
-		local tree_toggle = function(tree_opts)
-			require("nvim-tree").setup(tree_opts)
-			require("nvim-tree.api").tree.toggle()
-		end
-
-		vim.keymap.set("n", "<leader>tt", function()
-			opts.view = {
-				side = "right",
-				signcolumn = "yes",
-				width = 36,
-			}
-			tree_toggle(opts)
-		end, { desc = "Toggle file tree (right side)" })
-
-		vim.keymap.set("n", "<leader>tf", function()
-			local HEIGHT_RATIO = 0.8
-			local WIDTH_RATIO = 0.25
-
-			opts.view = {
-				signcolumn = "no",
-				float = {
-					enable = true,
-					open_win_config = function()
-						local screen_w = vim.opt.columns:get()
-						local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-						local window_w = screen_w * WIDTH_RATIO
-						local window_h = screen_h * HEIGHT_RATIO
-						local window_w_int = math.floor(window_w)
-						local window_h_int = math.floor(window_h)
-						local center_x = (screen_w - window_w) / 2
-						local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
-						return {
-							border = "rounded",
-							relative = "editor",
-							row = center_y,
-							col = center_x,
-							width = window_w_int,
-							height = window_h_int,
-						}
-					end,
-				},
-				width = function()
-					return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+		view = {
+			signcolumn = "no",
+			float = {
+				enable = true,
+				open_win_config = function()
+					local screen_w = vim.opt.columns:get()
+					local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+					local window_w = screen_w * 0.25
+					local window_h = screen_h * 0.8
+					local window_w_int = math.floor(window_w)
+					local window_h_int = math.floor(window_h)
+					local center_x = (screen_w - window_w) / 2
+					local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+					return {
+						border = "rounded",
+						relative = "editor",
+						row = center_y,
+						col = center_x,
+						width = window_w_int,
+						height = window_h_int,
+					}
 				end,
-			}
-			tree_toggle(opts)
-		end, { desc = "Toggle file tree (floating)" })
-	end,
+			},
+			width = function()
+				return math.floor(vim.opt.columns:get() * 0.25)
+			end,
+		},
+	},
+	keys = {
+		{
+			"<leader>tt",
+			"<cmd>NvimTreeToggle<cr>",
+			desc = "Toggle file tree",
+		},
+	},
 }
