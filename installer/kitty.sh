e166568ac7f2585667a088417b68c259a083ffd4
Hugo Englund <hugo.englund7@gmail.com>
2025-04-25 14:26:16 +0200

    :horse:

--- installer/kitty.sh
+++ installer/kitty.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
-sudo ln -s ~/.local/kitty.app/bin/kitty /usr/local/bin/kitty
-kitty +kitten themes catppuccin-mocha
+ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+
 kitty --version
+kitten themes catppuccin-mocha
