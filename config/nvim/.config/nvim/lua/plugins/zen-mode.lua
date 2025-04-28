6da01429306342a17dabf383e48256b7d463a215
Hugo Englund <hugo.englund7@gmail.com>
2025-04-28 12:01:00 +0200

    :horse:

--- config/nvim/.config/nvim/lua/plugins/zen-mode.lua
+++ config/nvim/.config/nvim/lua/plugins/zen-mode.lua
@@ -1,11 +1,8 @@
 return {
 	"folke/zen-mode.nvim",
-	dependencies = {
-		"folke/twilight.nvim",
-	},
 	opts = {},
 	keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Toggle Zen Mode" } },
 	plugins = {
-		twilight = { enabled = true },
+		twilight = { enabled = false },
 	},
 }
