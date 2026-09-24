67c5f417c634b5943e7f021d9f50c5694412b5f1
rickyhugo <hugo.englund@anyfin.com>
2026-09-24 14:11:14 +0200

    :horse:

--- nvim/lua/config/lsp-autocmd.lua
+++ nvim/lua/config/lsp-autocmd.lua
@@ -36,3 +36,23 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		end, { buffer = true, desc = "Go to next diagnostic [LSP]" })
 	end,
 })
+
+-- INFO: zig
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.zig", "*.zon" },
+	callback = function(_)
+		vim.lsp.buf.code_action({
+			context = { only = { "source.fixAll" } },
+			apply = true,
+		})
+	end,
+})
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.zig", "*.zon" },
+	callback = function(_)
+		vim.lsp.buf.code_action({
+			context = { only = { "source.organizeImports" } },
+			apply = true,
+		})
+	end,
+})
---
+++ nvim/plugin/zig.lua
@@ -0,0 +1,3 @@
+vim.pack.add({
+	"https://codeberg.org/ziglang/zig.vim",
+})
